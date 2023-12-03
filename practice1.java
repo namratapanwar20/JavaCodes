3374a6ea8baf1cb3d960ad3f26bb6549ccf98cc5
Namrata Panwar <116093745+namratapanwar20@users.noreply.github.com>
2023-12-03 00:18:43 -0600

    Add files via upload

---
+++ practice1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class practice1 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        String name=sc.next();
+        System.out.print("Hello "+name+", have a good day!");
+    }
+}
