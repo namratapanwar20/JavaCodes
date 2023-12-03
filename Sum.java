cfab3d13b2db4be7d0089538e46a490c7c0ee00a
Namrata Panwar <116093745+namratapanwar20@users.noreply.github.com>
2023-12-02 23:54:40 -0600

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,7 @@
+public class Sum {
+    public static void main(String[] args){
+        int a=4,b=5,c=6;
+        int sum=a+b+c;
+        System.out.println("Sum of three numbers : "+ sum);
+    }
+}
